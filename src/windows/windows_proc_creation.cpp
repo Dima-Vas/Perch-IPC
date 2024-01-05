7dbfc80b29d9a1851a510335fa4b454113d8744b
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2024-01-05 20:26:57 +0200

    Update windows_proc_creation.cpp

--- src/windows/windows_proc_creation.cpp
+++ src/windows/windows_proc_creation.cpp
@@ -8,12 +8,6 @@
 #include <string>
 #include "windows_proc_creation.h"
 
-
-/*
-    An auxillary POSIX-compatible function for launching a process represented by Process object.
-    Acts as an end-point for launching all subprocesses.
-*/
-
 std::wstring ConvertToWideString(const std::string& str) {
     int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], static_cast<int>(str.size()), nullptr, 0);
     std::wstring wstrTo(size_needed, 0);
@@ -21,7 +15,6 @@ std::wstring ConvertToWideString(const std::string& str) {
     return wstrTo;
 }
 
-// Function to convert std::vector<std::string> to LPWSTR* for Windows
 LPWSTR* ConvertToLPWSTRArray(const std::vector<std::string>& args) {
     LPWSTR* argv = new LPWSTR[args.size() + 1];
     int argc = args.size();
@@ -39,15 +32,12 @@ void launchChild(Process& process) {
     std::wstring executablePath = ConvertToWideString(process.getPath());
     std::vector<std::string> arguments = process.getArguments();
     LPWSTR* argv = ConvertToLPWSTRArray(arguments);
-
     std::wstring args;
     for (int i = 0; argv[i] != nullptr; ++i) {
         args += std::wstring(argv[i]) + L" ";
     }
-
     STARTUPINFOW si = {};
     PROCESS_INFORMATION pi = {};
-
     if (!CreateProcessW(
             NULL,
             &args[0],
@@ -62,8 +52,6 @@ void launchChild(Process& process) {
     )) {
         std::cerr << "Error: CreateProcess failed. Error code: " << GetLastError() << std::endl;
     }
-
-    // Clean up allocated memory for LPWSTR*
     for (int i = 0; argv[i] != nullptr; ++i) {
         delete[] argv[i];
     }
@@ -73,74 +61,41 @@ void launchChild(Process& process) {
 int windowsLaunch(Process& process) {
     launchChild(process);
     return 0;
-//    pid_t pid = fork();
-//
-//    if (pid < 0) {
-//        std::cerr << "Error: Fork failed." << std::endl;
-//        return -1;
-//    }
-//
-//    if (pid == 0) { // Child
-//        launchChild(process);
-//        exit(EXIT_FAILURE);
-//    } else { // Parent
-//        int status;
-//        process.setPID(pid);
-//        // std::cout << "in linuxLaunch : " << process.getPID() << std::endl;
-//        return pid;
-//    }
 }
 
 int windowsKill(Process& process, UINT killSig = CTRL_C_EVENT) {
     DWORD pid = static_cast<DWORD>(process.getPID());
-
-    // Open a handle to the process
     HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
     if (hProcess == NULL) {
         std::cerr << "Error: Unable to open process. Error code: " << GetLastError() << std::endl;
         return -1;
     }
-
-    // Terminate the process
     if (!TerminateProcess(hProcess, killSig)) {
         std::cerr << "Error: Unable to terminate process. Error code: " << GetLastError() << std::endl;
         CloseHandle(hProcess);
         return 1;
     }
-
     std::cout << "Killed: " << pid << std::endl;
-
-    // Close the process handle
     CloseHandle(hProcess);
-
     return 0;
 }
 
 int windowsWaitForExit(Process& process) {
     DWORD pid = static_cast<DWORD>(process.getPID());
-
-    // Open a handle to the process
     HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, pid);
     if (hProcess == NULL) {
         std::cerr << "Error: Unable to open process. Error code: " << GetLastError() << std::endl;
         return -1;
     }
-
-    // Wait for the process to exit
     DWORD waitResult = WaitForSingleObject(hProcess, INFINITE);
     if (waitResult != WAIT_OBJECT_0) {
         std::cerr << "Error: WaitForSingleObject failed. Error code: " << GetLastError() << std::endl;
         CloseHandle(hProcess);
         return 1;
     }
-
-    // Get the exit code of the process
     DWORD exitCode = 0;
     GetExitCodeProcess(hProcess, &exitCode);
-
-    // Close the process handle
     CloseHandle(hProcess);
-
     return static_cast<int>(exitCode);
 }
 
@@ -151,35 +106,25 @@ int* windowsPipeRedirectOutput(Process& out_process, Process& in_process, const 
     saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
     saAttr.bInheritHandle = TRUE;
     saAttr.lpSecurityDescriptor = NULL;
-
     HANDLE g_hChildStd_OUT_Rd = NULL;
     HANDLE g_hChildStd_OUT_Wr = NULL;
-
-    // Create a pipe for the child process's STDOUT
     if (!CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0)) {
         std::cerr << "Error creating output pipe" << std::endl;
         return nullptr;
     }
-
-    // Ensure the read handle to the pipe for STDOUT is not inherited.
     if (!SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0)) {
         std::cerr << "Error setting handle information" << std::endl;
         return nullptr;
     }
-
-    // Create the child process.
     PROCESS_INFORMATION piProcInfo;
     STARTUPINFOA siStartInfo;
     BOOL bSuccess = FALSE;
-
     ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
     ZeroMemory(&siStartInfo, sizeof(STARTUPINFOA));
     siStartInfo.cb = sizeof(STARTUPINFOA);
     siStartInfo.hStdError = g_hChildStd_OUT_Wr;
     siStartInfo.hStdOutput = g_hChildStd_OUT_Wr;
     siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
-
-    // Start the child process.
     bSuccess = CreateProcessA(
             NULL,
             const_cast<LPSTR>(out_process.getPath().c_str()),  // Executable path (as LPSTR)
@@ -192,17 +137,12 @@ int* windowsPipeRedirectOutput(Process& out_process, Process& in_process, const 
             &siStartInfo,
             &piProcInfo
     );
-
     if (!bSuccess) {
         std::cerr << "Error creating process: " << GetLastError() << std::endl;
         return nullptr;
     }
-
-    // Close handles to the process and thread
     CloseHandle(piProcInfo.hProcess);
     CloseHandle(piProcInfo.hThread);
-
-    // Return file descriptors
     int* pipe_fd = new int[2];
     pipe_fd[0] = _open_osfhandle(reinterpret_cast<intptr_t>(g_hChildStd_OUT_Rd), 0);
     pipe_fd[1] = _open_osfhandle(reinterpret_cast<intptr_t>(g_hChildStd_OUT_Wr), 0);
